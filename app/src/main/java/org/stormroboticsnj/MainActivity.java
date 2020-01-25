ac259f0b9e4e77a21756bf6e0c900b25423f2871
113020 <32312357+113020@users.noreply.github.com>
2020-01-25 14:50:14 -0500

    Small changes, new fragment sort teams

--- app/src/main/java/org/stormroboticsnj/MainActivity.java
+++ app/src/main/java/org/stormroboticsnj/MainActivity.java
@@ -1,24 +1,17 @@
 package org.stormroboticsnj;
 
 import android.os.Bundle;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
 import android.view.View;
-
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
 import com.google.android.material.navigation.NavigationView;
-
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
