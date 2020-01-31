93d3746a069d9ff8920df91358756a9b82baf2cc
Ryan Bohinski <rjbohinski@gmail.com>
2020-01-30 21:31:06 -0500

    Fix android lint errors
    https://stackoverflow.com/questions/58662901/lifecycleowner-difference-between-fragment-self-and-fragmentviewlifecycle

--- app/src/main/java/org/stormroboticsnj/ui/share/ShareFragment.java
+++ app/src/main/java/org/stormroboticsnj/ui/share/ShareFragment.java
@@ -24,7 +24,7 @@ public class ShareFragment extends Fragment {
                 ViewModelProviders.of(this).get(ShareViewModel.class);
         View root = inflater.inflate(R.layout.fragment_share, container, false);
         final TextView textView = root.findViewById(R.id.text_share);
-        shareViewModel.getText().observe(this, new Observer<String>() {
+        shareViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
